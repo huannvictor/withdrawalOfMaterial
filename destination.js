02e3158e7243032ab6fb6bfea934dea6ac00eb7a
Huann Almeida <huannvictor@gmail.com>
2021-12-29 11:23:14 -0300

    set date button

--- destination.js
+++ destination.js
@@ -3,27 +3,28 @@
 const submitButton = document.querySelector("#submitButton");
 let stateSearch = document.querySelector("#state");
 let citySearch = document.querySelector("#city");
+let dateSearch = document.querySelector("#date");
 let state;
 let city;
-
-// console.log(stateSearch);
-// console.log(citySearch);
+let date;
 
 submitButton.addEventListener("click", run);
 
 function run(event) {
   event.preventDefault();
-
+  
   stateSearch = stateSearch.value;
-
+  
   citySearch = citySearch.value;
   citySearch = citySearch.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   citySearch = citySearch.replace(" ", "%20");
 
+  date = dateSearch.value
+  console.log(date)
+
   axios
     .get(`https://viacep.com.br/ws/${stateSearch}/${citySearch}/rua/json/`)
     .then(function (response) {
-      
       if (response.data.error) {
         throw new Error();
       }
